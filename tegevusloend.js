2fc639c9b340515b065ed6844290260a9347a9b1
mbrikker <noreply>
2014-05-27 14:09:52 +0000

    Add module Tegevusloend

---
+++ tegevusloend.js
@@ -0,0 +1,16 @@
+/**
+ * @module //tegevusloend
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Tegevusloend
+ * @extends Montage
+ */
+exports.Tegevusloend = Montage.specialize(/** @lends Tegevusloend# */ {
+    constructor: {
+        value: function Tegevusloend() {
+            this.super();
+        }
+    }
+});
